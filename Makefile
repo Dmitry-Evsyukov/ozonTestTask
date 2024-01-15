68dc52f060fb3aad68a1ff4a30fddbec968044d9
dmitry <edp032017@yandex.ru>
2024-01-15 11:44:08 +0300

    add gomock for repo and UC, and add test for uC

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 .PHONY: docker-build docker-run-in-memory docker-run-postgres unit-test
 
 unit-test:
-
+	mockgen -source=internal/url/repository.go  -destination=internal/url/mock/mock_repository.go -package=mock
+	mockgen -source=internal/url/usecase.go  -destination=internal/url/mock/mock_usecase.go -package=mock
+	go test ./...
 
 docker-build:
 	docker build -t ozon/base -f docker/base/Dockerfile .
--- internal/url/usecase/usecase_test.go
+++ internal/url/usecase/usecase_test.go
@@ -1,1 +1,64 @@
 package urlUseCase
+
+import (
+	"context"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"main/internal/models"
+	"main/internal/url/mock"
+	"testing"
+)
+
+func TestUrlUC_SaveShortToFullUrl_Success(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUrlRepo := mock.NewMockRepository(ctrl)
+	urlUC := New(mockUrlRepo)
+
+	originalUrl := models.Url{Url: "https://example.com"}
+
+	mockUrlRepo.EXPECT().SaveShortToFullUrl(context.Background(), gomock.Any(), originalUrl.Url).Return(nil)
+
+	shortUrl, err := urlUC.CreateShortUrlAndSave(context.Background(), originalUrl)
+	require.NoError(t, err)
+	require.Nil(t, err)
+	require.NotEqual(t, "", shortUrl.Url)
+}
+
+func TestUrlUC_SaveShortToFullUrl_BadRequest(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUrlRepo := mock.NewMockRepository(ctrl)
+	urlUC := New(mockUrlRepo)
+
+	originalUrl := models.Url{Url: "example.com"}
+
+	shortUrl, err := urlUC.CreateShortUrlAndSave(context.Background(), originalUrl)
+	require.Error(t, err)
+	require.Equal(t, "", shortUrl.Url)
+}
+
+func TestUrlUC_GetFullUrl_Success(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUrlRepo := mock.NewMockRepository(ctrl)
+	urlUC := New(mockUrlRepo)
+
+	shortUrl := models.Url{Url: "short_url"}
+	originalUrl := "http://example.com"
+
+	mockUrlRepo.EXPECT().GetFullUrl(context.Background(), shortUrl.Url).Return(originalUrl, nil)
+
+	originalUrlFromUC, err := urlUC.GetFullUrl(context.Background(), shortUrl)
+	require.NoError(t, err)
+	require.Equal(t, originalUrl, originalUrlFromUC.Url)
+}
